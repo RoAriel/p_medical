77c8950835d0cd5592d7984c6c8d1b474627ad70
Rodrigo Gonzalez <roariel06@gmail.com>
2023-12-12 22:57:51 -0300

    Se editan scripts y el .md agregando la info del proyecto
    Se crea DER usando mysql

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# p_medical
+# MIS_MEDICAL
+Proyecto final para el curso de [CODERHOUSE - SQL](https://www.coderhouse.com/online/sql)
+## Descripción del Modelo
+Este modelo representa el sistema de asignación de turnos en una agenda médica de una prepaga.
+
+
+## DER (Diagrama entidad-relación)
+![DER de MIS_MEDICAL](DER/MIS_MEDICA.png)
+### Schema
+El schema donde se aloja el modelo será el p_medical
+
+
+
+
+|        Tabla        |          Descripción                      
+|---------------------|------------------------------------|
+|`Partner`            |Socios (afiliados) de la prepaga    |
+|`Doctor`             |Doctores de la prepaga              |
+|`Branch`             |Sucursales                          |
+|`Shift`              |Turnos médicos                      |
+|`Plan`               |Planes que ofrece la prepaga        |
+|`Medical_history`    | Historial médico del socio         |
+|`Medical_speciality` |Especialidades médicas de la prepaga|
--- create_tables.sql
+++ create_tables.sql
@@ -1,32 +1,33 @@
--- Tabla del plan que va tener el socio dentro de la prepaga
+USE p_medical;
+
 CREATE TABLE plan(
 id INT AUTO_INCREMENT
 , name VARCHAR(15) NOT NULL
-, description VARCHAR(100) NOT NULL
+, description ENUM ('B1','B2','EM')
 , PRIMARY KEY(`id`)
 );
 
--- Tabla de socios
 CREATE TABLE partner(
 id INT AUTO_INCREMENT
 , dni INT NOT NULL  
 , f_name VARCHAR(25) NOT NULL
 , l_name VARCHAR(25) NOT NULL
-, id_plan VARCHAR(10) NOT NULL
+, plan_id INT
+, address VARCHAR(30) NOT NULL
+, phone VARCHAR(25) NOT NULL
+, email VARCHAR(25)
 , PRIMARY KEY(`id`)
 , FOREIGN KEY(`id`) REFERENCES `plan` (`id`)
 );
 
--- Historial medico de los socios
 CREATE TABLE medical_history(
 id INT AUTO_INCREMENT
-, id_partner INT NOT NULL
-, md_history VARCHAR(150)
+, partner_id INT
+, md_history LONGTEXT
 , PRIMARY KEY(`id`)
 , FOREIGN KEY(`id`) REFERENCES `partner` (`id`)
 );
 
--- Tabla de especialidades medicas
 CREATE TABLE medical_speciality(
 id INT AUTO_INCREMENT
 , doctor_id INT NOT NULL
@@ -34,31 +35,29 @@ id INT AUTO_INCREMENT
 , PRIMARY KEY(`id`)
 );
 
--- Tabla de medicos
 CREATE TABLE doctor(
 id INT AUTO_INCREMENT
 , f_name VARCHAR(25) NOT NULL
 , l_name VARCHAR(25) NOT NULL
-, id_speciality INT
+, speciality_id INT
 , PRIMARY KEY(`id`)
 , FOREIGN KEY(`id`) REFERENCES `medical_speciality` (`id`)
 );
 
--- Tabla de sucursales
 CREATE TABLE branch(
 id INT AUTO_INCREMENT
 , name VARCHAR(25) NOT NULL
 , location VARCHAR(20) NOT NULL
 , address VARCHAR(35) NOT NULL
+, phone VARCHAR(25) NOT NULL
 , PRIMARY KEY(`id`)
 );
 
--- Tabla de turnos
 CREATE TABLE shift(
 id INT AUTO_INCREMENT
-, partner_id INT NOT NULL
-, doctor_id INT NOT NULL
-, branch_id INT NOT NULL
+, partner_id INT
+, doctor_id INT
+, branch_id INT
 , date_s DATE NOT NULL
 , time_s TIME NOT NULL
 , PRIMARY KEY(`id`)
