636dde1d6ddbe980cbbfa73a50114fe5e4156069
rodrigo gonzalez <roariel06@gmail.com>
2024-02-24 18:31:27 -0300

    Add sp para traer sucursales por zona

--- object_model/04_create_stored_procedures.sql
+++ object_model/04_create_stored_procedures.sql
@@ -34,5 +34,21 @@ begin
 end //
 DELIMITER ;
 
+ DROP PROCEDURE IF EXISTS sp_banch_per_zone;
+ DELIMITER //
+ create procedure sp_banch_per_zone (in the_zone VARCHAR(20))
+begin
+	
+     set @qry = CONCAT('SELECT name, location, address, phone from p_medical.branch where lower(location) = ', lower(the_zone));
+    -- set @qry = CONCAT('SELECT name, location, address, phone from p_medical.branch ;');
+    PREPARE runSql FROM @qry;
+    EXECUTE runSql;
+    DEALLOCATE PREPARE runSql;
+    
+end //
+DELIMITER ;
+
+ -- call sp_banch_per_zone('Zona Norte')
+
 -- call sp_add_or_del_medSpeciality('INS',null,'Oncologo');
 -- call sp_add_or_del_medSpeciality('DEL',16,''); -- en este ejemplo el 16 es el que se agrega en la linea de arriba
